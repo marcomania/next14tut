fb9a077add54c18e6e47a2e7921fa9cb5a808d93
Marco Untiveros <marco.untiveros@unmsm.edu.pe>
2023-11-26 00:39:03 +0000

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css'
+import { inter } from '@/app/ui/fonts';
+import Footer from './ui/footer';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/ui/footer.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Footer() {
+
+    return (
+        <footer>
+            Pie de Pagina 
+        </footer>
+    );
+}
