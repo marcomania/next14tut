1b245ad8f0f1a082b3d511613f3801495060c7ef
Marco Untiveros <marco.untiveros@unmsm.edu.pe>
2023-11-28 19:15:33 +0000

    feat: :sparkles: Adding Invoices CRUD
    CRUD mutations using server actions.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,9 +1,9 @@
 import { Revenue } from './definitions';
 
 export const formatCurrency = (amount: number) => {
-  return (amount / 100).toLocaleString('en-US', {
+  return (amount / 100).toLocaleString('es-PE', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'PEN',
   });
 };
 
